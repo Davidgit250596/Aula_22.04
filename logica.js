b6426f21745a968d1a503b17633bdee564bf989b
David Michael <davidmichael252@gmail.com>
2025-04-22 17:06:24 -0300

    Aula 22/04

---
+++ logica.js
@@ -0,0 +1,112 @@
+//variaveis
+
+// let nome = "João"
+// let idade = 100;
+// let tamanho = 200;
+// let aprovado = true;
+
+// console.log("somar idade + tamanho =", idade + tamanho);
+
+// console.log("subtraindo idade - tamanho =", idade - tamanho);
+
+// console.log("multiplicando idade x tamanho", idade * tamanho);
+
+// console.log("divisão idade + tamanho = ", idade / tamanho);
+
+// console.log("resto de idade + tamanho", idade % tamanho);
+
+
+// console.log("300>301 =", 5 > 3);
+// console.log("300<301 =", 5 < 3);
+// console.log("300>=299 =", 5 >= 3);
+// console.log("300<=310 =", 5 <= 3);
+// console.log("200!=300 =", 5 != 3);
+
+
+// console.log("aluno e idade", nome + " "+ idade);
+
+
+// && e
+// || ou
+// ! not
+
+// let idade = 80;
+
+// console.log(! (idade >= 18));
+
+// let idade = 19 ;
+// let TemConvite = true;
+
+// console.log(idade >=18 && TemConvite == true);
+
+
+// let idade = 19;
+// let aprovado = true;
+
+// if (idade >= 18 && idade <= 70 && aprovado == true) {
+//     console.log("Pode tirar a CNH");
+// }
+// else{
+//     console.log("Não pode tirar a CNH");
+
+// }
+
+// let idade = ;
+// let aprovado = true;
+
+// if (idade <10 ) {
+//     console.log("joazinho é uma criança");
+// }
+// else if (idade >=10 && idade< 15){
+//     console.log("Joaozinho é pré um adolescente");    
+// }
+// else if (idade >= 15 && idade <18){
+//     console.log("Joãozinho se tornou um adolescente)");    
+// }
+// else if (idade >=18 && idade <= 60){
+//     console.log("Joãozinho é um adulto");    
+// }
+// else{
+//     console.log("Seu jão é um velho rabujento");
+
+// }
+
+// let dinheiro = 50 ;
+// let semdinheiro = true;
+
+// if (dinheiro >=50){
+//     console.log("podera fazer sua compra");
+// }
+// else  {
+//     console.log("Não podera fazer sua compra");
+
+// }
+
+
+// let sexo = "masculino";
+// let idade = 17;
+
+// if(sexo == "masculino" && idade>=18){
+//     console.log("pode se alistar")
+// }else{
+//     console.log("não pode")
+// }
+
+let tipoUsuario = "editor";
+
+if (tipoUsuario == "admin") {
+    console.log("Acesso total");
+
+}
+else if (tipoUsuario == "editor") {
+    console.log("Acesso limitado");
+
+}
+else if (tipoUsuario == "visitantes") {
+    console.log("acesso somente para leitura");
+
+}
+else {
+    console.log("tipo de usuario invalido");
+
+}
